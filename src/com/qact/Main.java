61aa5ac337d206393eb94c4037600247f985897c
Quetzal Torres <quetzalt322@gmail.com>
2022-01-24 20:00:25 -0800

    Works now

--- src/com/qact/Main.java
+++ src/com/qact/Main.java
@@ -6,6 +6,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -15,40 +16,91 @@ public class Main {
     static BufferedImage image;
     static int imgWidth;
     static int imgHeight;
-    static final String luminance = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+    static int res;
+    static String luminanceScale  = "";
+    static final String lum1 = "@MBHENR#KWXDFdet[})=~!-/_';,:`. ";
+    static final String lum2 = "@%#WXFdt*+=~!-;,:`. ";
+    static final String lum3 = "@%#*+=-:. ";
+    static final String lum4 = "@0=: ";
     public static void main(String[] args) throws IOException {
+        boolean inApp = true;
+        while(inApp) {
+            //Get the directory for image
             do {
+                image = null;
                 System.out.println("Enter Image Directory");
                 directory = scanner.nextLine();
                 inputFile = new File(directory);
                 try {
                     image = ImageIO.read(inputFile);
-                }catch (Exception e){
-                    if(!inputFile.exists()){
+                } catch (Exception e) {
+                    if (!inputFile.exists()) {
                         System.out.println("Error: could not find a file at " + directory);
                     }
-                    if(image == null && inputFile.exists()){
+                    if (image == null && inputFile.exists()) {
                         System.out.println("Error: " + inputFile.getName() + " is not a valid image file");
                     }
                 }
 
-            } while (!inputFile.isFile()||image == null);
-            image = getScaledBWImage(image,128);
+            } while (!inputFile.isFile() || image == null);
+
+            //Get desired contrast level
+            do {
+                System.out.println("Enter Contrast level [1][2][3][4]");
+                switch (scanner.nextLine()) {
+                    case "1" -> {
+                        System.out.println("Set Contrast to Minimum(1)");
+                        luminanceScale = lum1;
+                    }
+                    case "2" -> {
+                        System.out.println("Set Contrast to 2");
+                        luminanceScale = lum2;
+                    }
+                    case "3" -> {
+                        System.out.println("Set Contrast to 3");
+                        luminanceScale = lum3;
+                    }
+                    case "4" -> {
+                        System.out.println("Set Contrast to Maximum(4)");
+                        luminanceScale = lum4;
+                    }
+                    default -> {
+                        System.out.println("Invalid Entry: please enter a number 1-4");
+                        luminanceScale = "";
+                    }
+                }
+            } while (luminanceScale.equals(""));
+            do {
+                System.out.println("Enter desired image width");
+                try {
+                    res = 0;
+                    res = scanner.nextInt();
+                }catch (InputMismatchException e){
+                    System.out.println("Invalid Entry: please enter an integer");
+                }
+            } while (res == 0);
+            image = getScaledBWImage(image, res);
             imgHeight = image.getHeight();
             imgWidth = image.getWidth();
-        System.out.println("Processing...");
-        System.out.println("Width: " + imgWidth);
-        System.out.println("Height: " + imgHeight);
-        System.out.println(Arrays.toString(image.getRaster().getPixels(0, 0, imgWidth, imgHeight, (int[]) null)));
-        System.out.println(pixelToAscii(image.getRaster().getPixels(0,0,imgWidth,imgHeight,(int[])null)));
+            System.out.println("Set image resolution to " + imgWidth +"x"+ imgHeight);
+            System.out.println("Press enter to generate image");
+            System.out.println("Processing...");
+            System.out.println(pixelToAscii(image.getRaster().getPixels(0,0,imgWidth,imgHeight,(float[])null)));
+            System.out.println("Create Another? [y/n]");
+            scanner.nextLine();
+            if (!scanner.nextLine().equalsIgnoreCase("y")){
+                inApp = false;
+                System.out.println("Quitting...");
+            }
+        }
     }
 
-    public static String pixelToAscii(int[] pixels){
+    public static String pixelToAscii(float[] pixels){
         String asciiImage = "";
-        for(int i = 0; i < imgHeight-1; i++){
-            for(int j = 0; j < imgWidth-1; i++) {
-                int currPix = i * (imgWidth);
-                asciiImage = asciiImage.concat(String.valueOf(luminance.charAt(pixels[currPix] / 255 * 70)));
+        for(int i = 0; i < pixels.length; i++){
+            asciiImage = asciiImage.concat(" " + luminanceScale.charAt((int) (pixels[i] / 255f * luminanceScale.length())) + " ");
+            if(i%imgWidth+1 == imgWidth){
+                asciiImage = asciiImage.concat("\n");
             }
         }
         return asciiImage;
